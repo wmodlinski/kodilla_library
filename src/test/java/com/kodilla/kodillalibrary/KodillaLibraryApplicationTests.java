d030f8bd35432007584bee3797c8a49eb9e8c2ee
Waldemar Modliński <103046767+wmodlinski@users.noreply.github.com>
2022-09-22 13:50:26 +0200

    0.1 - First Commit

---
+++ src/test/java/com/kodilla/kodillalibrary/KodillaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class KodillaLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
